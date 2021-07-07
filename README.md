ecacc7ca77781ccf07b7b6d3380194f738c87fc5
Nick Mudge <nick@perfectabstractions.com>
2021-07-07 09:46:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# contracts-starter
+Template for starting smart contract repos
---
+++ scripts/template.js
@@ -0,0 +1,15 @@
+/* global ethers */
+/* eslint-disable  prefer-const */
+
+async function main () {
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
