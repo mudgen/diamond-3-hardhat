08760ddfaee201ed99dcc0903435456b9aec2be6
byterose <byterose@tutanota.com>
2021-10-17 22:51:41 +0200

    Restore solidity version

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.6;
 
 /******************************************************************************\
 * Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
