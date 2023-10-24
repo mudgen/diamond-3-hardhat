bc8fa14736a7a445bf885687d5155d0269856a74
john j <143453887+JohnDeveloperJ@users.noreply.github.com>
2023-10-24 18:11:25 +0000

    git add.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,16 @@
-
 /* global ethers task */
-require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-waffle');
 require("@nomiclabs/hardhat-etherscan");
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async () => {
-  const accounts = await ethers.getSigners()
+  const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address)
+    console.log(account.address);
   }
-})
+});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -25,5 +25,16 @@ module.exports = {
       enabled: true,
       runs: 200
     }
+  },
+  networks: {
+    goerli: {
+      url: "YOUR_QUICKNODE_HTTP_ENDPOINT",
+      accounts: ["YOUR_PRIVATE_KEY"]
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "YOUR_ETHERSCAN_API_KEY"
   }
-}
+};
